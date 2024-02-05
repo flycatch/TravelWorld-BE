6d193024862f87645de31ab43d57db668d949f35
jithinvv4 <jithin.v@flycatchtech.com>
2024-02-05 15:25:55 +0530

    admin permission changes and updated migration file

--- api/admin.py
+++ api/admin.py
@@ -20,8 +20,8 @@ class AgentAdmin(CustomModelAdmin):
     list_filter = ("status", "stage")
     search_fields = ("username", "first_name", "last_name", "email", "phone")
 
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
 
 class UserAdmin(CustomModelAdmin):
@@ -38,8 +38,8 @@ class UserAdmin(CustomModelAdmin):
     list_editable = ("status",)
     search_fields = ("username", "first_name", "last_name", "email", "phone")
 
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
 
 class CountryAdmin(CustomModelAdmin):
@@ -97,8 +97,8 @@ class ActivityAdmin(CustomModelAdmin):
 
     inlines = [ActivityImageInline]
 
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
 
 class AttractionAdmin(CustomModelAdmin):
@@ -128,8 +128,8 @@ class PackageAdmin(CustomModelAdmin):
 
     inlines = [PackageImageInline]
 
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
 
 # Unregister model

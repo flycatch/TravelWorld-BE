d2ce70dbce04664fa6f4e571bacb809b0852cabe
lenate-flycatch <lenate.j@flycatchtech.com>
2024-02-19 19:29:28 +0530

    changes

--- api/utils/paginator.py
+++ api/utils/paginator.py
@@ -4,6 +4,6 @@ from rest_framework.response import Response
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 5  # Number of objects to return per page
+    page_size = 10  # Number of objects to return per page
     page_size_query_param = 'page_size'  # Custom query parameter to specify page size
     max_page_size = 100 

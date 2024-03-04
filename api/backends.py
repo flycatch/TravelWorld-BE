a205a234ee4abef8fbf0e487e6eb8b0fc31907e2
lenate-flycatch <lenate.j@flycatchtech.com>
2024-03-04 12:39:46 +0530

    model changes user and agent

--- api/backends.py
+++ api/backends.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from api.models import *
 
 class BaseUserModelBackend(ModelBackend):
     def authenticate(self, request, username=None, email=None, password=None, model=None, **kwargs):
@@ -16,10 +17,12 @@ class BaseUserModelBackend(ModelBackend):
                 pass
 
         if not user and username:
+            print(request)
             try:
                 user = UserModel.objects.get(username=username)
 
             except UserModel.DoesNotExist:
+                user = BaseUser.objects.get(unique_username=username)
                 pass
 
         if user and user.check_password(password):

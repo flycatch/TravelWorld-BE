09e4b5ed372984ce42e6f9e048f69f589453df31
lenate-flycatch <lenate.j@flycatchtech.com>
2024-03-12 13:58:18 +0530

    added

--- api/filters/review_filters.py
+++ api/filters/review_filters.py
@@ -5,7 +5,8 @@ from api.models import UserReview
 class ReviewFilter(django_filters.FilterSet):
     package = django_filters.CharFilter(field_name='package')
     activity = django_filters.CharFilter(field_name='activity')
+    booking = django_filters.CharFilter(field_name='booking')
 
     class Meta:
         model = UserReview
-        fields = ['package','activity']
+        fields = ['package','activity','booking']

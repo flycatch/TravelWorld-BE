6b17c203b20d13c39877bc17d84155a2d5373acb
jithinvv4 <jithin.v@flycatchtech.com>
2024-03-15 13:59:44 +0530

    destination, state multiselect api, itinerary model changes

--- api/v1/general/serializers.py
+++ api/v1/general/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Country, City, State
+from api.models import Country, City, State, Location
 
 
 class CountrySerializer(serializers.ModelSerializer):
@@ -18,3 +18,21 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ['id','name']
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    
+    class Meta:
+        model = Location
+        fields = ['id', 'country', 'state', 'destinations']
+
+
+# class LocationGetSerializer(serializers.ModelSerializer):
+#     id = serializers.IntegerField(required=False)
+#     destinations = CitySerializer(many=True, required=False)
+    
+#     class Meta:
+#         model = Location
+#         fields = ['id', 'country', 'state', 'destinations']
+
--- api/v1/general/viewsets.py
+++ api/v1/general/viewsets.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
-from api.models import Country, State, City
-from api.v1.general.serializers import CountrySerializer, StateSerializer, CitySerializer
+from api.models import Country, State, City, Location
+from api.v1.general.serializers import CountrySerializer, StateSerializer, CitySerializer, LocationSerializer
 from api.filters.general_filters import CityFilter
 
 
@@ -21,3 +23,17 @@ class CityViewSet(viewsets.ModelViewSet):
     serializer_class = CitySerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = CityFilter
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    http_method_names = ['get', 'delete']
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            'message': 'Location deleted successfully', 'status': 'success', 
+            'statusCode': status.HTTP_204_NO_CONTENT},
+            status=status.HTTP_204_NO_CONTENT)

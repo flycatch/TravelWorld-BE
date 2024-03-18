b334547203b83604e7194a736df636061fb9f4ff
lenate-flycatch <lenate.j@flycatchtech.com>
2024-03-19 00:11:52 +0530

    added filters in booking

--- api/v1/user/serializers.py
+++ api/v1/user/serializers.py
@@ -14,7 +14,7 @@ class UserBookingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id","username","first_name","last_name","email"]
+        fields = ["id","username","first_name","last_name","email","profile_image"]
 
 
 class UserSerializer(serializers.ModelSerializer):

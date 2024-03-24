ca1c4aaef6bf89a07407b7effaa4aae40c779841
jithinvv4 <jithin.v@flycatchtech.com>
2024-03-24 11:57:38 +0530

    product pricing filter, exclusion api update

--- api/v1/package/viewsets.py
+++ api/v1/package/viewsets.py
@@ -677,6 +677,9 @@ class HomePageProductsViewSet(viewsets.ReadOnlyModelViewSet):
         package_id = self.request.query_params.get('package')
         activity_id = self.request.query_params.get('activity')
 
+        price_range_min = self.request.query_params.get('price_range_min')
+        price_range_max = self.request.query_params.get('price_range_max')
+
         # Define Q objects to build complex filter conditions
         activity_filter = Q()
         package_filter = Q()
@@ -710,6 +713,13 @@ class HomePageProductsViewSet(viewsets.ReadOnlyModelViewSet):
         if is_popular:
             activity_filter &= Q(is_popular=True)
             package_filter &= Q(is_popular=True)
+
+        if price_range_min is not None and price_range_max is not None:
+            activity_filter &= Q(pricing_activity__adults_rate__gte=price_range_min) \
+            & Q(pricing_activity__adults_rate__lte=price_range_max)
+            package_filter &= Q(pricing_package__adults_rate__gte=price_range_min) \
+            & Q(pricing_package__adults_rate__lte=price_range_max)
+
 
         # Apply the combined filter conditions
         activities = self.queryset_activities.filter(activity_filter)

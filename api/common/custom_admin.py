ecaf53fda92f859ec2bfa1b17f8ea1b960027400
lenate-flycatch <lenate.j@flycatchtech.com>
2024-03-26 08:51:32 +0530

    added email change

--- api/common/custom_admin.py
+++ api/common/custom_admin.py
@@ -98,7 +98,7 @@ class PackageInformationsInline(CustomStackedInline):
     exclude = ['exclusiondetails', 'status']
     verbose_name = 'Information'
     verbose_name_plural = 'Information'
-    template = 'admin/information_tab.html'
+    # template = 'admin/information_tab.html'
 
     def has_change_permission(self, request, obj=None):
         return False
@@ -185,7 +185,7 @@ class ActivityInformationsInline(CustomStackedInline):
     exclude = ['exclusiondetails', 'status']
     verbose_name = 'Information'
     verbose_name_plural = 'Information'
-    template = 'admin/information_tab.html'
+    # template = 'admin/information_tab.html'
 
     def has_change_permission(self, request, obj=None):
         return False

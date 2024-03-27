b80dc35313e76be5047a1b67a2d67f9b58395d78
lenate-flycatch <lenate.j@flycatchtech.com>
2024-03-28 01:58:17 +0530

    added

--- api/models.py
+++ api/models.py
@@ -1213,7 +1213,7 @@ class CoverPageInput(AuditFields):
                                       verbose_name="Package")
     attraction_image = models.ImageField(upload_to='cover_images/', null=True, blank=True,
                                        verbose_name="Attraction")
-    price_min = models.IntegerField(null=True, blank=True)
+    price_min = models.IntegerField(null=True, blank=True,default=0)
     price_max = models.IntegerField(null=True, blank=True)
     
 

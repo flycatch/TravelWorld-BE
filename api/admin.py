86c74e2d5f02834dd75741a9c9052fdbbfab9865
lenate-flycatch <lenate.j@flycatchtech.com>
2024-04-03 13:04:31 +0530

    added

--- api/admin.py
+++ api/admin.py
@@ -1186,8 +1186,8 @@ class CoverPageInputAdmin(CustomModelAdmin):
         # }),
     )
 
-    def has_add_permission(self, request):
-        return True
+    # def has_add_permission(self, request):
+    #     return True
 
     def has_delete_permission(self, request, obj=None):
         return False

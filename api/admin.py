9fd48db1bca46a9f20e63b469a640b0b487c6c9a
lenate-flycatch <lenate.j@flycatchtech.com>
2024-04-03 13:01:29 +0530

    added

--- api/admin.py
+++ api/admin.py
@@ -1179,7 +1179,7 @@ class CoverPageInputAdmin(CustomModelAdmin):
             'fields': ("experience", "clients", "satisfaction")
         }),
         ('Cover Images', {
-            'fields': ("activity_image", "package_image", "attraction_image")
+            'fields': ("activity_image", "package_image", "attraction_image","product_image")
         }),
         # ('Filters', {
         #     'fields': ('price_min','price_max')

af94652e775ba381ed459f44ceaccb3b7836564b
jithinvv4 <jithin.v@flycatchtech.com>
2024-05-08 17:47:37 +0530

    agent - activity api listing serializer update

--- api/v1/package/serializers.py
+++ api/v1/package/serializers.py
@@ -25,13 +25,6 @@ class PackageSerializer(serializers.ModelSerializer):
         model = Package
         exclude = ['status', 'is_popular', "deal_type"]
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        # Add activities_data and suitable_for_data to the response data
-        data['activities'] = HomePageCategorySerializer(instance.activities.all(), many=True).data
-        data['suitable_for'] = HomePageSuitableForSerializer(instance.suitable_for.all(), many=True).data
-        return data
-    
     def validate(self, data):
         min_members = data.get('min_members')
         max_members = data.get('max_members')
@@ -88,6 +81,13 @@ class PackageSerializer(serializers.ModelSerializer):
                 instance.locations.add(locations_obj)
 
         return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Add activities_data and suitable_for_data to the response data
+        data['activities'] = HomePageCategorySerializer(instance.activities.all(), many=True).data
+        data['suitable_for'] = HomePageSuitableForSerializer(instance.suitable_for.all(), many=True).data
+        return data
     
 
 class PackageGetSerializer(serializers.ModelSerializer):

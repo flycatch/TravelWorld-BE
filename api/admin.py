ee8d8d825be7132900a5d4ec9beef7644a144275
jithinvv4 <jithin.v@flycatchtech.com>
2024-05-17 21:10:36 +0530

    cover image input add option removed

--- api/admin.py
+++ api/admin.py
@@ -1241,8 +1241,8 @@ class CoverPageInputAdmin(CustomModelAdmin):
         # }),
     )
 
-    # def has_add_permission(self, request):
-    #     return True
+    def has_add_permission(self, request):
+        return False
 
     def has_delete_permission(self, request, obj=None):
         return True

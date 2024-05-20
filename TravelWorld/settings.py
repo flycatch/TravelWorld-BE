bef6bcd082be71fd25258ab511f899dd9bc3e427
lenate-flycatch <lenate.j@flycatchtech.com>
2024-05-20 10:56:59 +0530

    added

--- TravelWorld/settings.py
+++ TravelWorld/settings.py
@@ -231,7 +231,7 @@ ADMIN_REORDER = (
     {'app': 'api', 
      'models': ('api.Country', 'api.State', 'api.City','api.Currency', 'api.AdvanceAmountPercentageSetting',
                 'api.CoverPageInput'
-                # 'api.Blogs','api.Pricing','api.BlogCategory','api.BlogImage'
+                'api.Blogs','api.Pricing','api.BlogCategory','api.BlogImage'
                 # 'api.Pricing','api.Itinerary',
                 # 'api.PackageCancellationCategory','api.CancellationPolicy','api.ActivityCancellationCategory','api.ActivityCancellationPolicy',
                 ),
